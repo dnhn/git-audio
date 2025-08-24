9a8cac3f9f6035e030a095ed5ce3eb255a6c78e9
nhan.dev <hey@nhan.dev>
2025-08-24 23:28:58 +0700

    Global commands

--- gca.sh
+++ gca.sh
@@ -3,17 +3,24 @@
 COMMAND="$1"
 PARAMETER="$2"
 
+CONFIG_DIR="$HOME/.git-audio"
+TEMPLATE_DIR="$CONFIG_DIR/templates"
+
 case "$COMMAND" in
-  init)
-    echo "init"
-    ;;
   global)
     case $PARAMETER in
-      commit)
-        echo "commit"
+      set)
+        git config --global init.templateDir $TEMPLATE_DIR
+        echo 'Global hook set.'
         ;;
-      reset)
-        echo "reset"
+      unset)
+        git config --global --unset init.templateDir
+        echo 'Global hook unset.'
+        ;;
+      *)
+        echo "\nGLOBAL HOOK COMMANDS"
+        echo "  set    \tSet global commit hook"
+        echo "  unset  \tUnset global commit hook"
         ;;
     esac
     ;;
